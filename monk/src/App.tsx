809e7dbbc695f5262a085df46f47efec0cb51ce2
George <gex7777@gmail.com>
2023-05-03 15:20:29 +0530

    add main section

--- monk/src/App.tsx
+++ monk/src/App.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Layout from "./components/Layout";
 import { themeOptions } from "./ulits/theme";
+import MainSection from "./MainSection";
 const theme = createTheme(themeOptions);
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Layout>
-          <></>
+          <MainSection />
         </Layout>
       </ThemeProvider>
     </>
