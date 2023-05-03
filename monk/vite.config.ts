3e317c347dc27d946938d7430a6935728fab257d
George <gex7777@gmail.com>
2023-05-03 15:18:55 +0530

    add svg plugin

--- monk/vite.config.ts
+++ monk/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+/// <reference types="vite-plugin-svgr/client" />
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [svgr(), react()],
+});
