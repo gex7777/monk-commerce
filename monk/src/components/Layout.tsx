73dc239f34c0800d065a652daa4a0bf28f4a5de9
George <gex7777@gmail.com>
2023-05-03 15:21:01 +0530

    create appbar

--- monk/src/components/Layout.tsx
+++ monk/src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
 
+import { ReactComponent as MonkLogo } from "../assets/logo.svg";
 interface Props {
   children: ReactNode;
 }
@@ -8,14 +9,39 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <Box sx={{ display: "flex" }}>
-      <AppBar position="fixed" sx={{ boxShadow: "none", borderBottom: "" }}>
-        <Toolbar></Toolbar>
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: "none",
+          borderBottom: "1px solid #D1D1D1;",
+          background: (theme) => theme.palette.background.default,
+        }}
+      >
+        <Toolbar style={{ minHeight: "45px" }}>
+          <MonkLogo height={30.97} />
+
+          <Typography
+            noWrap
+            component="div"
+            sx={{
+              fontStyle: "normal",
+              fontWeight: 600,
+              fontSize: 16,
+              lineHeight: 3,
+              marginLeft: 2,
+
+              display: { xs: "none", sm: "block" },
+              color: "#7E8185",
+            }}
+          >
+            Monk Upsell & Cross-sell
+          </Typography>
+        </Toolbar>
       </AppBar>
 
-      <Box component="main" sx={{ padding: 3 }}>
-        <Toolbar />
+      <Container maxWidth="sm" component="main" sx={{ mt: 8 }}>
         {children}
-      </Box>
+      </Container>
     </Box>
   );
 };
